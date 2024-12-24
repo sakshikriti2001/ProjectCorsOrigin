50bd75b9dd7f812573af8b615dfcfed1bddfbae2
sakshikriti2001 <msakshi2k1@gmail.com>
2024-12-24 11:06:45 +0530

    E:\projectCors

---
+++ src/main/java/com/example/cors/origin/projectCors/ProjectCorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cors.origin.projectCors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cors/origin/projectCors/ProjectCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cors.origin.projectCors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
